e715b73862f2c9d85f9c4d803915e2722eff0d25
popolens <noval.paulvincent@gmail.com>
2025-03-10 10:52:55 +0800

    Update settings.py

--- genesis/settings.py
+++ genesis/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -196,4 +199,6 @@ DJOSER = {
     "SERIALIZERS": {},
     "LOGIN_FIELD": "email",
 }
+
+
 
